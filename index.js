e9762ec046a05745c90f0466dca48f9679b0c783
Vaibhav Arora <82141868+VaibhavArora19@users.noreply.github.com>
2022-01-27 20:52:42 +0530

    Checking sth

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var totalButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < totalButtons; i++) {
+for (var i = 0; i <=totalButtons; i++) {
    document.querySelectorAll(".drum")[i].addEventListener("click", function()
    {
       makeSound(this.innerHTML);
